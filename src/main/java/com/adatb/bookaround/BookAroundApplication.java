60ac5056005b23ca51b510d30de5009338c4ccff
Ábel Bakk <abelbakk@gmail.com>
2023-03-01 16:11:34 +0100

    added project structure

---
+++ src/main/java/com/adatb/bookaround/BookAroundApplication.java
@@ -0,0 +1,13 @@
+package com.adatb.bookaround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adatb/bookaround/BookAroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adatb.bookaround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
