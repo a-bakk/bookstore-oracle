4d3b2ef492f3f5a6df316cb793c1e567a32069aa
Ocztos Károly <126339777+OcztosKaroly@users.noreply.github.com>
2023-03-25 13:10:11 +0100

    customers methods

--- src/main/java/com/adatb/bookaround/repositories/CustomerDao.java
+++ src/main/java/com/adatb/bookaround/repositories/CustomerDao.java
@@ -1,10 +1,13 @@
 package com.adatb.bookaround.repositories;
 
 import com.adatb.bookaround.entities.Customer;
+import com.adatb.bookaround.models.BookWithAuthorsAndGenres;
 import jakarta.persistence.NoResultException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
@@ -13,6 +16,12 @@ public class CustomerDao extends AbstractJpaDao<Customer> {
 
     private static final Logger logger = LogManager.getLogger(CustomerDao.class);
 
+
+    /**
+     * Keresés email alapján felhasználóra
+     * @param email felhasználó emailje
+     * @return az emailhoz tartozó profil
+     */
     public Customer findByEmail(String email) {
         if (email == null || email.isEmpty()) return null;
         Customer result;
@@ -26,4 +35,59 @@ public class CustomerDao extends AbstractJpaDao<Customer> {
         return result;
     }
 
+    /**
+     * Keresés felhasználóra id alapján
+     * @param id felhasználó idje
+     * @return az idhoz tartozó profil
+     */
+    public Customer findById(int id) {
+        Customer result;
+        try {
+            result = entityManager.createQuery("SELECT c FROM Customer c WHERE c.customerId = :id", Customer.class)
+                    .setParameter(id, id).getSingleResult();
+        } catch (NoResultException e) {
+            logger.warn("User could not be found with the following id: " + id);
+            return null;
+        }
+        return result;
+    }
+
+    /**
+     * Egyezik-e a két jelszó
+     * @param customer a felhasználó
+     * @param password ellenőrizendő jelszó
+     * @return ha jó a jelszó true, különben false
+     */
+    /*TODO: Change password to hashed password??*/
+    public boolean isPasswordCorrect(Customer customer, String password) {
+        if (password == null || password.isEmpty()) return false;
+        if (customer == null) return false;
+        return customer.getPassword().equals(password);
+    }
+
+    /**
+     * Összes felhasználó listázása
+     * @return Az összes felhasználóból alkotott lista.
+     */
+    public List<Customer[]> findAllCustomers() {
+
+        String jpql = "SELECT Customer FROM Customer";
+
+        List<Customer[]> resultList = entityManager.createQuery(jpql, Customer[].class)
+                .getResultList();
+
+        return resultList;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
