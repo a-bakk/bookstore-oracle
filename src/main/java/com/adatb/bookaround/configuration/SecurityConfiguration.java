2b265761319b87a9f368e4e419cb16624838088c
Ábel Bakk <abelbakk@gmail.com>
2023-04-18 19:06:45 +0200

    added authentication success handler

--- src/main/java/com/adatb/bookaround/configuration/SecurityConfiguration.java
+++ src/main/java/com/adatb/bookaround/configuration/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.adatb.bookaround.configuration;
 
+import com.adatb.bookaround.entities.Customer;
+import com.adatb.bookaround.repositories.CustomerDao;
 import com.adatb.bookaround.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -10,11 +13,17 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.time.LocalDateTime;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    CustomerDao customerDao;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -26,14 +35,24 @@ public class SecurityConfiguration {
         return new CustomerService();
     }
 
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return (request, response, authentication) -> {
+            Customer customer = customerDao.findByEmail(authentication.getName());
+            customer.setLastLogin(LocalDateTime.now());
+            customerDao.update(customer);
+            response.sendRedirect("/profile");
+        };
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin()
                 .loginPage("/auth")
-                .defaultSuccessUrl("/index", true)
+                .defaultSuccessUrl("/profile", true)
                 .failureUrl("/auth?error=true")
+                .successHandler(authenticationSuccessHandler())
                 .and()
                 .userDetailsService(userDetailsService())
                 .headers(headers -> headers.frameOptions().sameOrigin())
